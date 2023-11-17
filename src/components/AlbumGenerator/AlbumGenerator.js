f93cc4258a997dc20cd5c43004af3aca7b3b83b4
Jonathan Schwob <jrschwob@gmail.com>
2023-11-17 03:23:50 -0800

    fix: grey out download button

--- src/components/AlbumGenerator/AlbumGenerator.js
+++ src/components/AlbumGenerator/AlbumGenerator.js
@@ -3,21 +3,22 @@ import GeneratorTextList from "./GeneratorTextList";
 import GeneratorPhotoList from "./GeneratorPhotoList";
 
 const AlbumGenerator = ({ selectedPhotos, removePhoto, drop, download }) => {
+  const disableButton =
+    selectedPhotos.length === 0 ? "opacity-50 cursor-not-allowed" : "";
+
   return (
     <div
       ref={drop}
       className="ml-4 container border border-gray-500 p-4 bg-teal-400"
     >
-      <div className="flex items-center justify-between mb-4">
+      <div className="flex items-start justify-between mb-4">
         <h2 className="text-xl font-bold mb-4">Album Generator</h2>
-        {selectedPhotos.length > 0 && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={download}
-          >
-            Click to download album
-          </button>
-        )}
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold ml-4 py-1 px-2 rounded lg:py-2 lg:px-4 ${disableButton}`}
+          onClick={download}
+        >
+          Download album
+        </button>
       </div>
       <div className="flex flex-col lg:flex-row items-start">
         <GeneratorPhotoList
