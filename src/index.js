c31f4573735900e1bee762d1329014728e6b34cc
Jonathan Schwob <jrschwob@gmail.com>
2023-11-16 22:23:11 -0800

    chore: clean up old imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { DndProvider } from "react-dnd-multi-backend";
-// import { DndProvider } from "react-dnd";
-// import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import App from "./App";
 import { HTML5toTouch } from "rdndmb-html5-to-touch";
