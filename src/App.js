99691aa7e9c6754bbefb157472a00b873fce8725
Jonathan Schwob <jrschwob@gmail.com>
2023-11-17 02:20:09 -0800

    feat: download image as zip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import JSZip from "jszip";
 import { useEffect, useState } from "react";
+import { useDrop } from "react-dnd";
 import PhotoList from "./components/PhotoList/PhotoList";
 import Header from "./components/Header";
-import { useDrop } from "react-dnd";
 import AlbumGenerator from "./components/AlbumGenerator/AlbumGenerator";
 
 const App = () => {
@@ -36,6 +37,40 @@ const App = () => {
     },
   });
 
+  const downloadPhotosAsZip = async () => {
+    const zip = new JSZip();
+
+    // Create a folder in the zip file
+    const folder = zip.folder("photos");
+
+    // Fetch and add each selected photo to the zip file
+    const fetchAndAddToZip = async (photo, index) => {
+      try {
+        const response = await axios.get(photo.url, {
+          responseType: "arraybuffer",
+        });
+        const filename = `photo_${index + 1}.jpg`;
+        folder.file(filename, response.data);
+      } catch (error) {
+        console.error(`Error fetching photo ${index + 1}:`, error);
+      }
+    };
+
+    // Use Promise.all to fetch all photos concurrently
+    await Promise.all(selectedPhotos.map(fetchAndAddToZip));
+
+    // Generate the zip file
+    const content = await zip.generateAsync({ type: "blob" });
+
+    // Create a download link and trigger the download
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(content);
+    downloadLink.download = "photos.zip";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   const removeSelectedPhoto = (index) => {
     const updatedSelectedPhotos = [...selectedPhotos];
     updatedSelectedPhotos.splice(index, 1);
@@ -51,6 +86,7 @@ const App = () => {
           removePhoto={removeSelectedPhoto}
           selectedPhotos={selectedPhotos}
           drop={drop}
+          download={downloadPhotosAsZip}
         />
       </div>
     </div>
--- src/components/AlbumGenerator/AlbumGenerator.js
+++ src/components/AlbumGenerator/AlbumGenerator.js
@@ -2,13 +2,14 @@ import React from "react";
 import GeneratorTextList from "./GeneratorTextList";
 import GeneratorPhotoList from "./GeneratorPhotoList";
 
-const AlbumGenerator = ({ selectedPhotos, removePhoto, drop }) => {
+const AlbumGenerator = ({ selectedPhotos, removePhoto, drop, download }) => {
   return (
     <div
       ref={drop}
       className="ml-4 container border border-gray-500 p-4 bg-teal-400"
     >
       <h2 className="text-xl font-bold mb-4">Album Generator</h2>
+      <button onClick={download}>Click to download album</button>
       <div className="flex flex-col lg:flex-row items-start">
         <GeneratorPhotoList
           selectedPhotos={selectedPhotos}
