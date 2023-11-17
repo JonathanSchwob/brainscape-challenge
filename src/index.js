5aa84e56484769f989e23069692b9758d894d94d
Jonathan Schwob <jrschwob@gmail.com>
2023-11-16 22:22:52 -0800

    feat: add HTML5toTouch backend pipeline with dnd-multi-backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd-multi-backend";
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import App from "./App";
+import { HTML5toTouch } from "rdndmb-html5-to-touch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <DndProvider backend={HTML5Backend}>
+  <DndProvider options={HTML5toTouch}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
